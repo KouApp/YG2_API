d11ef14b1332b7d93e841123cb0b831474a71476
Yasin Şahin <hacklojen01@gmail.com>
2022-03-05 17:11:01 +0300

    example

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # key
 git clone https://yasinsahin0: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@github.com/KouApp/yaz1Api.git
+
+
+sql hatası
+
+https://github.com/mkleehammer/pyodbc/issues/938
+
+odbc kurulumu
+sudo su
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+exit
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get install -y unixodbc-dev
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import pyodbc
 print(pyodbc.__file__)
 db = pyodbc.connect(
-    'Driver={SQL Server};'
+    'Driver={ODBC Driver 17 for SQL Server};'
     'Server=sql.athena.domainhizmetleri.com;'
     'Database=abdullah_pys;'
     'UID=abdullah_pys;'
@@ -12,20 +12,4 @@ imlec.execute('SELECT * FROM [abdullah_pys].[test_table]')
 kullanicilar = imlec.fetchall()
 for i in kullanicilar:
     print(i)
-"""
-    DBMS: Microsoft
-    SQL
-    Server(ver.
-    15.00
-    .2080)
-    Case
-    sensitivity: plain = mixed, delimited = mixed
-    Driver: Microsoft
-    JDBC
-    Driver
-    9.4
-    for SQL Server(ver. 9.4.0.0, JDBC4.2)
-    Ping: 33
-    ms
-    
-"""
+
