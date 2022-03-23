e1e5ea1f799cc41f4f270f1031e7ad93d3231457
Yasin Şahin <56588137+yasinsahin0@users.noreply.github.com>
2022-03-23 05:08:56 +0300

    restore

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask,jsonify,request
 import db_insert
 import db_query
 
+# Fonksiyon isimlerini kontrol et
+# nesneden çağırılan metodları kontrol et
+# dönüş değerlerini kontrol et
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['JSON_SORT_KEYS'] = False
@@ -10,15 +13,161 @@ app.config['ENV'] = 'development'
 @app.route('/test',methods=['POST'])
 def test():
 	print('[INFO]--[test]--[FUNCTION]')
-	id = request.form['id']
+	faculty_id = request.form['faculty_id']
 	quer = db_query.Query()
-	response = quer.faculty_id_query(id)
+	response = quer.faculty_id_query(faculty_id)
 	return response
-@app.route('/test2',methods=['POST'])
-def test2():
+
+#student_id,advisior_id,name,surname,mail,phone_no,depart_id,faculty_id,clas,photo_path,password
+@app.route('/studentInsert',methods=['POST'])
+def studentInsert():
 	print('[INFO]--[test]--[FUNCTION]')
+	student_id = request.form['student_id']
+	advisior_id = request.form['advisior_id']
 	name = request.form['name']
-	return name
+	surname = request.form['surname']
+	mail = request.form['mail']
+	phone_no = request.form['phone_no']
+	depart_id = request.form['depart_id']
+	faculty_id = request.form['faculty_id']
+	clas = request.form['clas']
+	photo_path = request.form['photo_path']
+	password = request.form['password']
+	insert = db_insert.Database_insert()
+	result = insert.student_insert(student_id,advisior_id,name,surname,mail,phone_no,depart_id,faculty_id,clas,photo_path,password)
+	return result
+
+# faculty_id,name
+@app.route('/facultyInsert',methods=['POST'])
+def facultyInsert():
+	print('[INFO]--[test]--[FUNCTION]')
+	faculty_id = request.form['faculty_id']
+	name = request.form['name']
+	insert = db_insert.Database_insert()
+	result = insert.faculty_insert(faculty_id,name)
+	return result
+
+# depart_id,faculty_id,name
+@app.route('/departmentInsert',methods=['POST'])
+def departmentInsert():
+	print('[INFO]--[test]--[FUNCTION]')
+	faculty_id = request.form['faculty_id']
+	depart_id = request.form['depart_id']
+	name = request.form['name']
+	insert = db_insert.Database_insert()
+	result = insert.department_insert(depart_id,faculty_id,name)
+	return result
+
+# reg_id,name,surname,title,mail,depart_id,faculty_id,photo_path,password
+@app.route('/advisorInsert',methods=['POST'])
+def advisorInsert():
+	print('[INFO]--[test]--[FUNCTION]')
+	reg_id = request.form['reg_id']
+	name = request.form['name']
+	surname = request.form['surname']
+	title = request.form['title']
+	mail = request.form['mail']
+	depart_id = request.form['depart_id']
+	faculty_id = request.form['faculty_id']
+	photo_path = request.form['photo_path']
+	password = request.form['password']
+	insert = db_insert.Database_insert()
+	result = insert.advisor_insert(reg_id,name,surname,title,mail,depart_id,faculty_id,photo_path,password)
+	return result
+
+# advisor_id,student_id,status,message
+@app.route('/messageInsert',methods=['POST'])
+def messageInsert():
+	print('[INFO]--[test]--[FUNCTION]')
+	advisor_id = request.form['advisor_id']
+	student_id = request.form['student_id']
+	status = request.form['status']
+	message = request.form['message']
+	insert = db_insert.Database_insert()
+	result = insert.messsage_insert(advisor_id,student_id,status,message)
+	return result
+
+# id,projenumber,pdfpath,docpath,status,desc,insertdate,updatedate
+@app.route('/dissertationInsert',methods=['POST'])
+def dissertationInsert():
+	print('[INFO]--[test]--[FUNCTION]')
+	id = request.form['id']
+	projenumber = request.form['projenumber']
+	pdfpath = request.form['pdfpath']
+	docpath = request.form['docpath']
+	status = request.form['status']
+	desc = request.form['desc']
+	insertdate = request.form['insertdate']
+	updatedate = request.form['updatedate']
+	insert = db_insert.Database_insert()
+	result = insert.dissertation_insert(id,projenumber,pdfpath,docpath,status,desc,insertdate,updatedate)
+	return result
+
+# id,projenumber,pdfpath,docpath,status,desc,insertdate,updatedate
+@app.route('/reportsInsert',methods=['POST'])
+def reportsInsert():
+	print('[INFO]--[test]--[FUNCTION]')
+	id = request.form['id']
+	projenumber = request.form['projenumber']
+	pdfpath = request.form['pdfpath']
+	docpath = request.form['docpath']
+	status = request.form['status']
+	desc = request.form['desc']
+	insertdate = request.form['insertdate']
+	updatedate = request.form['updatedate']
+	insert = db_insert.Database_insert()
+	result = insert.reports_insert(id,projenumber,pdfpath,docpath,status,desc,insertdate,updatedate)
+	return result
+
+# id,mainprojeid,otherprojeid,plagrismrate
+@app.route('/plagiarismInsert',methods=['POST'])
+def plagiarismInsert():
+	print('[INFO]--[test]--[FUNCTION]')
+	id = request.form['id']
+	mainprojeid = request.form['mainprojeid']
+	otherprojeid = request.form['otherprojeid']
+	plagrismrate = request.form['plagrismrate']
+	insert = db_insert.Database_insert()
+	result = insert.plagiarism_insert(id,mainprojeid,otherprojeid,plagrismrate)
+	return result
+
+# id,startdate,enddate,name
+@app.route('/semesterInsert',methods=['POST'])
+def semesterInsert():
+	print('[INFO]--[test]--[FUNCTION]')
+	id = request.form['id']
+	startdate = request.form['startdate']
+	enddate = request.form['enddate']
+	name = request.form['name']
+	insert = db_insert.Database_insert()
+	result = insert.semester_insert(id,startdate,enddate,name)
+	return result
+
+# projects_insert  id,number,version,headline,matter,cont,purpose,keyword,metariel,method,poss,status,descr,maxplag,semeterid,studentid,insertiondate,updatedate
+@app.route('/projectsInsert',methods=['POST'])
+def projectsInsert():
+	print('[INFO]--[test]--[FUNCTION]')
+	id = request.form['id']
+	number = request.form['number']
+	version = request.form['version']
+	headline = request.form['headline']
+	matter = request.form['matter']
+	cont = request.form['cont']
+	purpose = request.form['purpose']
+	keyword = request.form['keyword']
+	metariel = request.form['metariel']
+	method = request.form['method']
+	poss = request.form['poss']
+	status = request.form['status']
+	descr = request.form['descr']
+	maxplag = request.form['maxplag']
+	semeterid = request.form['semeterid']
+	studentid = request.form['studentid']
+	insertiondate = request.form['insertiondate']
+	updatedate = request.form['updatedate']
+	insert = db_insert.Database_insert()
+	result = insert.projects_insert(id,number,version,headline,matter,cont,purpose,keyword,metariel,method,poss,status,descr,maxplag,semeterid,studentid,insertiondate,updatedate)
+	return result
 
 if __name__ == '__main__':
 	app.run()
--- db_insert.py
+++ db_insert.py
@@ -44,257 +44,280 @@ class Database_insert:
             return "Error code : 10"
         if not len(password) < 255:
             return "Error code : 11"
-        curs = self.db.cursor()
-        curs.execute("insert into m_Student(studentID,advisorID,name,surname,mail,phoneNumber,departmentID,facultyID,class,photoPath,password) values (?, ?,?,?,?,?,?,?,?,?,?)",
-            str(student_id),
-            int(advisior_id),
-            str(name),
-            str(surname),
-            str(mail),
-            str(phone_no),
-            str(depart_id),
-            str(faculty_id),
-            str(clas),
-            str(photo_path),
-            str(password))
-        curs.commit()
-        return "Succesfull"
+        try:
+            curs = self.db.cursor()
+            curs.execute(
+                "insert into m_Student(studentID,advisorID,name,surname,mail,phoneNumber,departmentID,facultyID,class,photoPath,password) values (?, ?,?,?,?,?,?,?,?,?,?)",
+                str(student_id),
+                int(advisior_id),
+                str(name),
+                str(surname),
+                str(mail),
+                str(phone_no),
+                str(depart_id),
+                str(faculty_id),
+                str(clas),
+                str(photo_path),
+                str(password))
+            curs.commit()
+            return "Successful"
+        except:
+            return "Critical : 1"
 
     def faculty_insert(self,faculty_id,name):
         table_c = tableControl.TableControl()
         if not table_c.faculty_id_control(faculty_id):
             if len(name) < 50:
-                curs = self.db.cursor()
-                curs.execute("insert into m_Faculty(facultyID, name) values (?, ?)", str(faculty_id), str(name))
-                curs.commit()
-                return "Succesfull"
+                try:
+                    curs = self.db.cursor()
+                    curs.execute("insert into m_Faculty(facultyID, name) values (?, ?)", str(faculty_id), str(name))
+                    curs.commit()
+                    return "Successful"
+                except:
+                    return "Critical : 2"
             else:
-                return "Error code : 13"
+                return "Error code : 12"
         else:
-            return "Error code : 12"
+            return "Error code : 13"
 
     def department_insert(self,depart_id,faculty_id,name):
         table_c = tableControl.TableControl()
         if not table_c.department_id_control(depart_id):
             if not table_c.faculty_id_control(faculty_id):
                 if len(name) < 55:
-                    curs = self.db.cursor()
-                    curs.execute("insert into m_Department(departmentID, facultyID, name) values (?, ?, ?)", str(depart_id), str(faculty_id),str(name))
-                    curs.commit()
-                    return "başarılı"
+                    try:
+                        curs = self.db.cursor()
+                        curs.execute("insert into m_Department(departmentID, facultyID, name) values (?, ?, ?)",
+                                     str(depart_id), str(faculty_id), str(name))
+                        curs.commit()
+                        return "Successful"
+                    except:
+                        return "Critical : 3"
                 else:
-                    return "Error code : 15"
+                    return "Error code : 14"
             else:
-                return "Error code : 12"
+                return "Error code : 15"
         else:
-            return "Error code : 14"
+            return "Error code : 16"
 
     def advisor_insert(self,reg_id,name,surname,title,mail,depart_id,faculty_id,photo_path,password):
         table_c = tableControl.TableControl()
         if not table_c.advisor_reg_control(reg_id):
-            return "Error code : 16"
-        if not len(name) < 105:
-            return "Error code : 3"
-        if not len(surname) < 50:
-            return "Error code : 4"
-        if not len(mail) < 255:
-            return "Error code : 5"
-        if not len(title) < 30:
             return "Error code : 17"
+        if not len(name) < 105:
+            return "Error code : 18"
+        if not len(surname) < 50:
+            return "Error code : 19"
+        if not len(mail) < 255:
+            return "Error code : 20"
+        if not len(title) < 30:
+            return "Error code : 21"
         if not table_c.department_id_control(depart_id):
-            return "Error code : 7"
+            return "Error code : 22"
         if not table_c.faculty_id_control(faculty_id):
-            return "Error code : 8"
+            return "Error code : 23"
         if not type(photo_path) == str:
-            return "Error code : 10"
+            return "Error code : 24"
         if not len(password) < 255:
-            return "Error code : 11"
-        curs = self.db.cursor()
-        curs.execute("insert into m_Advisor(registrationID,name,surname,title,mail,departmentID,facultyID,photoPath,password) values (?,?,?,?,?,?,?,?,?)",
-            str(reg_id),
-            str(name),
-            str(surname),
-            str(title),
-            str(mail),
-            str(depart_id),
-            str(faculty_id),
-            str(photo_path),
-            str(password))
-        curs.commit()
-        return "Succesfull"
+            return "Error code : 25"
+        try:
+            curs = self.db.cursor()
+            curs.execute("insert into m_Advisor(registrationID,name,surname,title,mail,departmentID,facultyID,photoPath,password) values (?,?,?,?,?,?,?,?,?)",
+                str(reg_id),
+                str(name),
+                str(surname),
+                str(title),
+                str(mail),
+                str(depart_id),
+                str(faculty_id),
+                str(photo_path),
+                str(password))
+            curs.commit()
+            return "Successful"
+        except:
+            return "Critical : 4"
 
     def messsage_insert(self,advisor_id,student_id,status,message):
         table_c = tableControl.TableControl()
         date = datetime.datetime.now()
         if table_c.advisor_reg_control(advisor_id):
-            return "Error code : 19"
+            return "Error code : 26"
         if table_c.student_id_control(student_id):
-            return "Error code : 20"
+            return "Error code : 27"
         if not type(status) == int:
-            return "Error code : 20"
+            return "Error code : 28"
         if not type(message) == str:
-            return "Error code : 21"
-        curs = self.db.cursor()
-        curs.execute("insert into t_message(advisorID,studentID,date,status,message) values (?,?,?,?,?)",
-            int(advisor_id),
-            str(student_id),
-            date,
-            int(status),
-            str(message))
-        curs.commit()
-        return "Succesfull"
+            return "Error code : 29"
+        try:
+            curs = self.db.cursor()
+            curs.execute("insert into t_message(advisorID,studentID,date,status,message) values (?,?,?,?,?)",
+                         int(advisor_id),
+                         str(student_id),
+                         date,
+                         int(status),
+                         str(message))
+            curs.commit()
+            return "Successful"
+        except:
+            return "Critical : 5"
 
     def dissertation_insert(self,id,projenumber,pdfpath,docpath,status,desc,insertdate,updatedate):
         if not type(id) == int:
-            return "Error code : 25"
-        if not type(projenumber) == str:
-            return "Error code : 26"
-        if not type(pdfpath) == str:
-            return "Error code : 27"
-        if not type(docpath) == str:
-            return "Error code : 28"
-        if not type(status) == int:
-            return "Error code : 29"
-        if not type(desc) == str:
             return "Error code : 30"
-
-        curs = self.db.cursor()
-        curs.execute("insert into m_Dissertation(id,projectNumber,pdfPath,docPath,status,description,insertionDate,updateDate) values (?,?,?,?,?,?,?,?)",
-                        id,
-                        projenumber,
-                        pdfpath,
-                        docpath,
-                        status,
-                        desc,
-                        insertdate,
-                        updatedate)
-        curs.commit()
-        return "Succesfull"
+        if not type(projenumber) == str:
+            return "Error code : 31"
+        if not type(pdfpath) == str:
+            return "Error code : 32"
+        if not type(docpath) == str:
+            return "Error code : 33"
+        if not type(status) == int:
+            return "Error code : 34"
+        if not type(desc) == str:
+            return "Error code : 35"
+        try:
+            curs = self.db.cursor()
+            curs.execute(
+                "insert into m_Dissertation(id,projectNumber,pdfPath,docPath,status,description,insertionDate,updateDate) values (?,?,?,?,?,?,?,?)",
+                id,
+                projenumber,
+                pdfpath,
+                docpath,
+                status,
+                desc,
+                insertdate,
+                updatedate)
+            curs.commit()
+            return "Successful"
+        except:
+            return "Critical : 6"
 
     def reports_insert(self,id,projenumber,pdfpath,docpath,status,desc,insertdate,updatedate):
         if not type(id) == int:
-            return "Error code : 31"
-        if not type(projenumber) == str:
-            return "Error code : 32"
-        if not type(pdfpath) == str:
-            return "Error code : 33"
-        if not type(docpath) == str:
-            return "Error code : 34"
-        if not type(status) == int:
-            return "Error code : 35"
-        if not type(desc) == str:
             return "Error code : 36"
+        if not type(projenumber) == str:
+            return "Error code : 37"
+        if not type(pdfpath) == str:
+            return "Error code : 38"
+        if not type(docpath) == str:
+            return "Error code : 39"
+        if not type(status) == int:
+            return "Error code : 40"
+        if not type(desc) == str:
+            return "Error code : 41"
+        try:
+            curs = self.db.cursor()
+            curs.execute(
+                "insert into t_reports(id,projectNumber,pdfPath,docPath,status,description,insertionDate,updateDate) values (?,?,?,?,?,?,?,?)",
+                id,
+                projenumber,
+                pdfpath,
+                docpath,
+                status,
+                desc,
+                insertdate,
+                updatedate)
+            curs.commit()
+            return "Successful"
+        except:
+            return "Critical : 7"
 
-        curs = self.db.cursor()
-        curs.execute("insert into t_reports(id,projectNumber,pdfPath,docPath,status,description,insertionDate,updateDate) values (?,?,?,?,?,?,?,?)",
-                        id,
-                        projenumber,
-                        pdfpath,
-                        docpath,
-                        status,
-                        desc,
-                        insertdate,
-                        updatedate)
-        curs.commit()
-        return "Succesfull"
 
     def plagiarism_insert(self,id,mainprojeid,otherprojeid,plagrismrate):
         if not type(id) == int:
-            return "Error code : 37"
+            return "Error code : 42"
         if not type(mainprojeid) == int:
-            return "Error code : 38"
+            return "Error code : 43"
         if not type(otherprojeid) == int:
-            return "Error code : 39"
+            return "Error code : 44"
         if not type(plagrismrate) == int:
-            return "Error code : 40"
-        curs = self.db.cursor()
-        curs.execute("insert into t_Plagiarism(id,mainProjeID,otherProjeID,plagiarismRate) values (?,?,?,?)",
-                        id,
-                        mainprojeid,
-                        otherprojeid,
-                        plagrismrate)
-        curs.commit()
-        return "Succesfull"
+            return "Error code : 45"
+        try:
+            curs = self.db.cursor()
+            curs.execute("insert into t_Plagiarism(id,mainProjeID,otherProjeID,plagiarismRate) values (?,?,?,?)",
+                         id,
+                         mainprojeid,
+                         otherprojeid,
+                         plagrismrate)
+            curs.commit()
+            return "Successful"
+        except:
+            return "Critical : 8"
+
 
     def semester_insert(self,id,startdate,enddate,name):
         if not type(id) == int:
-            return "Error code : 41"
+            return "Error code : 46"
         if not type(name) == str:
-            return "Error code : 42"
-        curs = self.db.cursor()
-        curs.execute("insert into m_semester(id,startDate,endDate,name) values (?,?,?,?)",
-                        id,
-                        startdate,
-                        enddate,
-                        name)
-        curs.commit()
-        return "Succesfull"
+            return "Error code : 47"
+        try:
+            curs = self.db.cursor()
+            curs.execute("insert into m_semester(id,startDate,endDate,name) values (?,?,?,?)",
+                         id,
+                         startdate,
+                         enddate,
+                         name)
+            curs.commit()
+            return "Successful"
+        except:
+            return "Critical : 9"
 
     def projects_insert(self,id,number,version,headline,matter,cont,purpose,keyword,metariel,method,poss,status,descr,maxplag,semeterid,studentid,insertiondate,updatedate):
         if not type(id) == int:
-            return "Error code : 41"
+            return "Error code : 48"
         if not type(number) == str:
-            return "Error code : 42"
+            return "Error code : 49"
         if not type(version) == int:
-            return "Error code : 41"
+            return "Error code : 50"
         if not type(headline) == str:
-            return "Error code : 42"
+            return "Error code : 51"
         if not type(matter) == str:
-            return "Error code : 41"
+            return "Error code : 52"
         if not type(cont) == str:
-            return "Error code : 42"
+            return "Error code : 53"
         if not type(purpose) == str:
-            return "Error code : 41"
+            return "Error code : 54"
         if not type(keyword) == str:
-            return "Error code : 42"
+            return "Error code : 55"
         if not type(metariel) == str:
-            return "Error code : 42"
+            return "Error code : 56"
         if not type(method) == str:
-            return "Error code : 41"
+            return "Error code : 57"
         if not type(poss) == str:
-            return "Error code : 42"
+            return "Error code : 58"
         if not type(status) == int:
-            return "Error code : 42"
+            return "Error code : 59"
         if not type(descr) == str:
-            return "Error code : 41"
+            return "Error code : 60"
         if not type(maxplag) == str:
-            return "Error code : 42"
+            return "Error code : 61"
         if not type(semeterid) == int:
-            return "Error code : 42"
+            return "Error code : 62"
         if not type(studentid) == str:
-            return "Error code : 41"
-        curs = self.db.cursor()
-        curs.execute("insert into t_Projects(id,number,version,headline,matter,[content],purpose,keyword,materiel,method,possibility,status,description,maxPlagiarism,semesterID,studentID,insertionDate,updatedDate) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-                        id,
-                        number,
-                        version,
-                        headline,
-                         matter,
-                         cont,
-                         purpose,
-                         keyword,
-                         metariel,
-                         method,
-                         poss,
-                         status,
-                         descr,
-                         maxplag,
-                         semeterid,
-                         studentid,
-                         insertiondate,
-                         updatedate
-                     )
-        curs.commit()
-        return "Succesfull"
-    #Project tablosunda content ne ?
-
-dbase = Database_insert()
-#print(dbase.advisor_insert(3,"mert","taş","baslık","mail","10","1","5","paswrd"))
-#a = dbase.student_insert("116",1,"aaaa","bbbb","awda","5123","10","1","12","addaw","aad")
-#print(a)
-#b = dbase.faculty_insert("2","bilisim")
-#print(b)
-date = datetime.datetime.now()
-#dbase.projects_insert(5,"23awd",1,"headln","matter","contetnt","purpose","keyword","metariel","method","poss",5,"desc","maxplag",0,"111",date,date)
-
+            return "Error code : 63"
+        try:
+            curs = self.db.cursor()
+            curs.execute("insert into t_Projects(id,number,version,headline,matter,[content],purpose,keyword,materiel,method,possibility,status,description,maxPlagiarism,semesterID,studentID,insertionDate,updatedDate) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                            id,
+                            number,
+                            version,
+                            headline,
+                             matter,
+                             cont,
+                             purpose,
+                             keyword,
+                             metariel,
+                             method,
+                             poss,
+                             status,
+                             descr,
+                             maxplag,
+                             semeterid,
+                             studentid,
+                             insertiondate,
+                             updatedate
+                         )
+            curs.commit()
+            return "Successful"
+        except:
+            return "Critical : 10"
 
